27dc2d42899802285832825a88cf34fb55f06ad4
PatrickBett <patrickbett018@gmail.com>
2023-11-10 07:18:38 +0300

    post the comments

--- src/components/Itemdetail.jsx
+++ src/components/Itemdetail.jsx
@@ -1,16 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Itemdetail({ rec }) {
   const [name, setName] = useState('');
   const [comment, setComment] = useState('');
-  const [comments, setComments] = useState([]);
 
-  useEffect(() => {
-    // Fetch comments when the component mounts
-   
-  }, []);
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault();
 
- 
+    try {
+      const response = await axios.post('http://localhost:3001/comments', {
+        name,
+        comment,
+        recipeLabel: rec.recipe.label,
+      }) ;
+     alert('Comment submitted successfully')
+
+      console.log(response.data); // log the response from the server
+      // You can update the UI or take any other necessary actions here
+    } catch (error) {
+      console.error('Error submitting comment:', error);
+    }
+  };
 
   return (
     <div className='ingredients-section'>
@@ -20,26 +31,27 @@ export default function Itemdetail({ rec }) {
           <li>{ingredient}</li>
         </ul>
       ))}
-      <form className='comment-form'>
-        <input type='text'   placeholder='Enter Your Name...' />
+      <form className='comment-form' onSubmit={handleCommentSubmit}>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder='Enter Your Name...'
+        />
         <br />
         <textarea
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
           placeholder='Type your comment about the ingredients here...'
-          
-          
         ></textarea>
         <br />
-        <button className='commentBtn' >
+        <button type='submit' className='commentBtn'>
           Submit
         </button>
       </form>
 
       <h2>Comments:</h2>
-      {/* {comments.map((comment, index) => (
-        <p key={index} className='added-comment'>
-          <strong>{comment.name}: </strong> {comment.comment}
-        </p>
-      )} */}
+      {/* Display existing comments if needed */}
     </div>
   );
 }
